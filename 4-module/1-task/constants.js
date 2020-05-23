e8f3ff40e106caa6aa647cae8a8e77bfe01bd62d
dmitrii <ffklffkl@gmail.com>
2020-05-23 17:15:53 +0300

    Solution 1-task

---
+++ 4-module/1-task/constants.js
@@ -0,0 +1,7 @@
+const path = require('path');
+
+const FILES_ROOT = path.join(__dirname, 'files');
+
+module.exports = {
+  FILES_ROOT,
+};
--- 4-module/1-task/server.js
+++ 4-module/1-task/server.js
@@ -1,23 +1,28 @@
 const url = require('url');
 const http = require('http');
-const path = require('path');
+const filesProcessor = require('./files-processor');
 
 const server = new http.Server();
 
-server.on('request', (req, res) => {
-  const pathname = url.parse(req.url).pathname.slice(1);
+server.on('request', async (req, res) => {
+  try {
+    const pathSegments = url.parse(req.url).pathname.slice(1).split('/');
 
-  const filepath = path.join(__dirname, 'files', pathname);
-
-  switch (req.method) {
-    case 'GET':
-
-      break;
-
-    default:
-      res.statusCode = 501;
-      res.end('Not implemented');
+    switch (req.method) {
+      case 'GET':
+        await filesProcessor.getFile(pathSegments, res);
+        break;
+      default:
+        res.statusCode = 501;
+        res.end('Not implemented');
+    }
+  } catch (err) {
+    console.error(err);
+    res.statusCode = 500;
+    res.end('Internal Error');
   }
 });
+
+server.on('error', (err) => console.error(err));
 
 module.exports = server;
