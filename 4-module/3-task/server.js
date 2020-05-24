4d2495201e6907aaa2006cea424a4c71d1a8582c
dmitrii <ffklffkl@gmail.com>
2020-05-24 22:56:32 +0300

    3-task solution

--- 4-module/3-task/server.js
+++ 4-module/3-task/server.js
@@ -1,17 +1,30 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
-
   const filepath = path.join(__dirname, 'files', pathname);
+
+  if (pathname.includes('/') || pathname.includes('..')) {
+    res.statusCode = 400;
+    res.end('Nested paths are not allowed');
+    return;
+  }
 
   switch (req.method) {
     case 'DELETE':
-
+      fs.unlink(filepath, (err) => {
+        if (err) {
+          res.statusCode = 404;
+          res.end('File Not Found');
+        } else {
+          res.end('File deleted successfully');
+        }
+      });
       break;
 
     default:
