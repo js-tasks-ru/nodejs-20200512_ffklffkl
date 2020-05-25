e5c3ed0c340493c4a7c07fa7c72d295bdd77da95
dmitrii <ffklffkl@gmail.com>
2020-05-25 23:33:38 +0300

    2-task some fixes, compact code, 3-task 500 err fix

--- 4-module/2-task/server.js
+++ 4-module/2-task/server.js
@@ -1,72 +1,53 @@
 const url = require('url');
 const http = require('http');
+const {STATUS_CODES} = require('http');
 const path = require('path');
 const fs = require('fs');
 const LimitSizeStream = require('./LimitSizeStream');
 
 const server = new http.Server();
 
+const SIZE_LIMIT = 1024 * 1024;
+const NOOP = () => undefined;
+
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
   const filepath = path.join(__dirname, 'files', pathname);
 
-  if (pathname.includes('/') || pathname.includes('..')) {
-    res.statusCode = 400;
-    res.end('Nested paths are not allowed');
-    return;
-  }
-
-  if (req.headers['content-length'] === '0') {
-    res.statusCode = 409;
-    res.end();
-    return;
-  }
+  if (pathname.includes('/') || pathname.includes('..')) return sendResponse(400);
+  if (req.headers['content-length'] > SIZE_LIMIT) return sendResponse(413);
 
   switch (req.method) {
     case 'POST':
-      const limitSize = new LimitSizeStream({limit: 1024 * 1024});
-      const writeFile = fs.createWriteStream(filepath, {flags: 'wx'});
-
-      limitSize.on('error', (err) => {
-        fs.unlink(filepath, () => {});
-        errorHandler(err);
-      });
-
-      writeFile
-          .on('error', (err) => errorHandler(err));
-
       req
-          .on('aborted', () => fs.unlink(filepath, () => {}))
-          .on('error', (err) => errorHandler(err))
-          .pipe(limitSize)
-          .pipe(writeFile)
-          .on('finish', () => {
-            res.statusCode = 201;
-            res.end('Success');
-          });
+          .on('aborted', () => fs.unlink(filepath, NOOP))
+          .on('error', (err) => handleError(err))
+          .pipe(new LimitSizeStream({limit: SIZE_LIMIT}))
+          .on('error', (err) => fs.unlink(filepath, () => handleError(err)))
+          .pipe(fs.createWriteStream(filepath, {flags: 'wx'}))
+          .on('error', (err) => handleError(err))
+          .on('finish', () => sendResponse(201));
       break;
     default:
-      res.statusCode = 501;
-      res.end('Not implemented');
+      sendResponse(501);
   }
 
-  function errorHandler(err) {
+  function handleError(err) {
     switch (err.code) {
-      case 'EEXIST': {
-        res.statusCode = 409;
-        res.end('Conflict');
+      case 'EEXIST':
+        sendResponse(409);
         break;
-      }
-      case 'LIMIT_EXCEEDED': {
-        res.statusCode = 413;
-        res.end('Limit exceeded');
+      case 'LIMIT_EXCEEDED':
+        sendResponse(413);
         break;
-      }
-      default: {
-        res.statusCode = 500;
-        res.end('Internal server error');
-      }
+      default:
+        sendResponse(500);
     }
+  }
+
+  function sendResponse(code) {
+    res.statusCode = code;
+    res.end(STATUS_CODES[code]);
   }
 });
 
--- 4-module/3-task/server.js
+++ 4-module/3-task/server.js
@@ -19,8 +19,13 @@ server.on('request', (req, res) => {
     case 'DELETE':
       fs.unlink(filepath, (err) => {
         if (err) {
-          res.statusCode = 404;
-          res.end('File Not Found');
+          if (err.code === 'ENOENT') {
+            res.statusCode = 404;
+            res.end('Not found');
+          } else {
+            res.statusCode = 500;
+            res.end('Internal error');
+          }
         } else {
           res.end('File deleted successfully');
         }
