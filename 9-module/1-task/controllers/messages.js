91d54a2a140bf1ff020fcd6e3cb4bb7103af4270
dmitrii <ffklffkl@gmail.com>
2020-06-12 20:58:10 +0300

    9-module 1-task solution

--- 9-module/1-task/controllers/messages.js
+++ 9-module/1-task/controllers/messages.js
@@ -1,5 +1,14 @@
 const Message = require('../models/Message');
 
 module.exports.messageList = async function messages(ctx, next) {
-  ctx.body = {messages: []};
+  ctx.body = {
+    messages: await Message
+        .find({chat: ctx.user._id})
+        .limit(20)
+        .then((m) => m.map(mapMessage)),
+  };
 };
+
+function mapMessage({_id, text, date, user}) {
+  return {text, date, user, id: _id};
+}
